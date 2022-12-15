db2f11ae4543a315df5333a67bd563b4da142b67
Shidqon Famulaqih <shidqon.famulaqih@bukalapak.com>
2022-12-15 10:00:18 +0700

    has_and_belongs_to_many birds trees

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,4 +1,6 @@
 class Bird < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 255 }
+  has_and_belongs_to_many :trees
+  validates_associated :trees
 end
